d8a32c8c52fdfb5a8e044a14ee2ebf9004abd4b0
anthony mcglone <ronaldoshaky@yahoo.com>
2014-10-07 19:54:22 +0100

    adding more unit tests

--- classes/account.py
+++ classes/account.py
@@ -5,8 +5,13 @@ class Account:
 	self.balance = balance
 
     def withdraw(self, withdrawal_amount):
+	if withdrawal_amount > self.balance:
+	    raise Exception 
 	self.balance = self.balance - withdrawal_amount
 
     def get_balance(self):
 	return self.balance	
+    
+    def deposit(self, deposit_amount):
+	self.balance = self.balance + deposit_amount	
 
--- classes/u_tests_account.py
+++ classes/u_tests_account.py
@@ -13,5 +13,15 @@ class ATMOperations(unittest2.TestCase):
 	f.withdraw(500)
 	self.assertEquals(500, f.get_balance())
 
+    def test_withdraw_operation_raises_exception_if_withdrawal_exceeds_balance(self):
+	f = account.Account("Anthony","McG",1000)
+	self.assertRaises(Exception, f.withdraw, 1500)
+
+    def test_deposit_operatione(self):
+	f = account.Account("Anthony","McG",1000)
+	f.deposit(500)
+	self.assertEquals(1500, f.get_balance())
+
+
 if __name__ == '__main__':
     unittest2.main()
